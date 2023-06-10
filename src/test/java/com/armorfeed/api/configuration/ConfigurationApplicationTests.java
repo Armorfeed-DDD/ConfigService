f7a720d2cbbd2f506040bc775b49a8be2f4d2a55
De La Mar <delamarzp@gmail.com>
2023-06-10 09:24:31 -0500

    feat: add configuration notification service

--- src/test/java/com/armorfeed/api/configuration/ConfigurationApplicationTests.java
+++ src/test/java/com/armorfeed/api/configuration/ConfigurationApplicationTests.java
@@ -1,5 +1,6 @@
 package com.armorfeed.api.configuration;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,6 +11,10 @@ class ConfigurationApplicationTests {
 	void contextLoads() {
 		int p=1;
 		p=p+1;
+	}
+	@Test
+	void dd(){
+
 	}
 
 }
